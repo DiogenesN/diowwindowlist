3c0848bba3d33195f3c832c62faf227cd1b16f3b
Nicolas <147919918+DiogenesN@users.noreply.github.com>
2024-05-01 12:38:42 +0300

    Delete src directory

--- src/getstrfromsubstrinfile.h
+++
@@ -1,4 +0,0 @@
-#ifndef GETSTRFROMSUBSTRINFILE_H_
-#define GETSTRFROMSUBSTRINFILE_H_
-	char *find_substring_in_file(const char *fullPathToConf, const char *substring);
-#endif
--- src/getvaluefromconf.h
+++
@@ -1,8 +0,0 @@
-#ifndef GETINTVALUEFROMCONF_H_
-#define GETINTVALUEFROMCONF_H_
-
-	int get_int_value_from_conf(char *fullPathToConf, char *nameToGetValueFrom);
-	float get_double_value_from_conf(char *fullPathToConf, char *nameToGetValueFrom);
-	char *get_char_value_from_conf(char *fullPathToConf, char *nameToGetValueFrom);
-
-#endif
